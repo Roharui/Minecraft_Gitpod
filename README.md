1d8a7c1251e2901946ec09be67152f97391631f6
Roharui <jack555586@gmail.com>
2020-07-11 14:51:57 +0000

    Build Base

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Minecraft_Gitpod
+
+I just want to build Minecraft plugin (or mods) at gitpod
---
+++ podding/src/main/java/minecraft/App.java
@@ -0,0 +1,12 @@
+package minecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin
+{
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        System.out.println("[GitPod] is successfuly build Minecraft plugin!");
+    }
+}
