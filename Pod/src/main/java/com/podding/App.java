1f1a267a960921687c5abe60d64016e1a360d9cd
Roharui <jack555586@gmail.com>
2020-07-12 03:26:53 +0000

    Build check

--- Pod/src/main/java/com/podding/App.java
+++ Pod/src/main/java/com/podding/App.java
@@ -1,17 +1,21 @@
 package com.podding;
 
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Hello world!
  */
-public class App {
-    private App() {
+
+public class App extends JavaPlugin{
+
+    private final Logger logger = Logger.getLogger("Minecraft");
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        this.logger.info("[Pod] is been enabled");
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
 }
